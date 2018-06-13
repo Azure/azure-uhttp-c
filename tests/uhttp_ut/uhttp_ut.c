8ed02a1052686c9880dc1be35521badeb12c65d5
Jelani <jelani.brandon@microsoft.com>
2018-06-13 05:09:22 +0000

    Updating submodules to the latest

--- tests/uhttp_ut/uhttp_ut.c
+++ tests/uhttp_ut/uhttp_ut.c
@@ -17,6 +17,10 @@
 #include "umock_c_negative_tests.h"
 #include "umocktypes_bool.h"
 
+#if defined _MSC_VER
+#pragma warning(disable: 4054) /* MSC incorrectly fires this */
+#endif
+
 static void* my_gballoc_malloc(size_t size)
 {
     return malloc(size);
