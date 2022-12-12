4db1235ad2e9adde9a31d7e1389cf40084edc342
Jose Sanchez <108306325+JoseSanchez7@users.noreply.github.com>
2022-12-12 14:00:21 -0800

    Add parameters to function call

--- src/uhttp.c
+++ src/uhttp.c
@@ -377,7 +377,7 @@ static int write_text_line(HTTP_CLIENT_HANDLE_DATA* http_data, const char* text_
         result = 0;
         if (http_data->trace_on)
         {
-            log_data_line;
+            log_data_line(http_data, text_line);;
         }
     }
     return result;
